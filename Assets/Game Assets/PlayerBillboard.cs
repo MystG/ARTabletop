7ba7da166a66d34f9bb553097da11dd925a08de5
Alexander Christner <christner.alex@gmail.com>
2018-12-10 19:09:02 -0500

    added billboards to player and machines to display their recources

---
+++ Assets/Game Assets/PlayerBillboard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBillboard : MonoBehaviour {
+
+    private Inventory inv;
+    private Text text;
+
+    // Use this for initialization
+    void Start () {
+        inv = GetComponentInParent<Inventory>();
+        text = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = "HP:" + inv.GetValue((int)Inventory.Indexes.HP)
+                    + " E:" + inv.GetValue((int)Inventory.Indexes.Energy)
+                    + " Coins:" + inv.GetValue((int)Inventory.Indexes.Coins);
+    }
+}
